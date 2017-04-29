76c21b55ec3158e74ee52b70322b96fdff403e51
basked <basket.baza@gmail.com>
2017-04-30 00:00:58 +0300

    События

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,15 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
 
+    public void onRedAction(ActionEvent actionEvent) {
+    }
+
+    public void onGreenAction(ActionEvent actionEvent) {
+    }
+
+    public void onYellowAction(ActionEvent actionEvent) {
+    }
 }
