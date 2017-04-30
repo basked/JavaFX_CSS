69826b3e904c72ce1a1bbb79d38da2b65b04ae99
basked <basket.baza@gmail.com>
2017-04-30 23:57:15 +0300

    Форма редактирования для справочника и обработка события в контроллере

---
+++ src/bas/telphone/JavaFX/controllers/EditDialogController.java
@@ -0,0 +1,7 @@
+package bas.telphone.JavaFX.controllers;
+
+/**
+ * Created by basket on 30.04.2017.
+ */
+public class EditDialogController {
+}
--- src/bas/telphone/JavaFX/controllers/MainController.java
+++ src/bas/telphone/JavaFX/controllers/MainController.java
@@ -1,7 +1,39 @@
 package bas.telphone.JavaFX.controllers;
 
+import com.sun.xml.internal.ws.api.client.SelectOptimalEncodingFeature;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import sun.nio.ch.sctp.SctpStdSocketOption;
 
+import java.io.IOException;
+
+/**
+ * Created by basket on 30.04.2017.
+ */
 public class MainController {
+    // добавление нового
+    public void ShowDialog(ActionEvent actionEvent) throws IOException {
+        try {
+            Stage stage = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("../fxml/edit.fxml"));
+            stage.setTitle("Редактирование записи");
+            stage.setMinHeight(95);
+            stage.setMinHeight(71);
+            stage.setResizable(false); // нельзя растянуть
+            stage.setScene(new Scene(root));
+            stage.initModality(Modality.WINDOW_MODAL); // указываем что окно будет модальным
+            // без InitOner окно не будет модельным
+            stage.initOwner(((Node) actionEvent.getSource()).getScene().getWindow()); //определяем родительское окно через свойства actionEvent . источник . сцена источника. главное окно
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
+    }
 }
